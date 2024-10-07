10c3a50602b816fb930c545965e4682a489021c0
Reyhan Sabian <81471883+SabianDev@users.noreply.github.com>
2024-10-07 12:29:20 +0700

    upload base file
    upload index.php , css, dan jss bootstrap projek

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="bg-light">
+    
+    <div class="container mt-3" style="margin: 20px 50px;">
+        <h1>Form Project</h1> <br>
+        <form>
+            
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 1">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 2">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 3">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 4">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 5">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 6">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 7">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 8">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 9">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Textbox 10">
+            </div>
+            <div class="ctn-button">
+                <input type="submit" class="btn btn-primary"></button>
+                <button class="btn btn-secondary">Cancel</button>
+            </div>
+            
+        </form>
+    </div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
