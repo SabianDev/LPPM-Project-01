983e09da877da507bbce217940b4ae5c513a32bf
Reyhan Sabian <sabianreyhan@gmail.com>
2024-10-15 12:25:08 +0700

    edit desain login jadi lebih baru -fitroh

--- login.php
+++ login.php
@@ -3,57 +3,47 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- link css  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/login-style.css">
-
-    <!-- link icons  -->
-    <script src="https://kit.fontawesome.com/59784df54a.js" crossorigin="anonymous"></script>
-    <title>Halaman Login</title>
+    <title>Login</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="github-logo">
-            <img src="img/logopiksi.png" alt="Logo Piksi" style="width: 100px; height: auto;"> <!-- Perbesar ukuran gambar -->
-        </div>
-        <h1 class="github-head">
-           Welcome to SIPEDAS
-        </h1>
-        <div class="login-wrapper">
-            <form action="mainmenu.php" method="post">
-                <div class="input-box">
-                    <div class="input-label">Username</div>
-                    <input type="text" placeholder="Masukkan username" required>
-                </div>
 
-                <div class="input-box">
-                    <div class="input-label">
-                        <span>Password</span>
-                        <a href="#">Forgot password?</a>
-                    </div>
-                    <div class="password-wrapper">
-                        <input type="password" id="password" placeholder="Masukkan password" required>
-                        <i class="fa fa-eye toggle-password" id="togglePassword"></i>
-                    </div>
+    <div class="container" id="container">
+        <div class="form-container sign-in">
+            <form action="mainmenu.php" method="post" onsubmit="return validateForm()">
+                <div class="logo">
+                    <img src="img/logo_sipedas.png" alt="Logo Sipedas" width="130" height="130">
                 </div>
-            
-                <button class="submit-btn" type="submit">
-                    Masuk
-                </button>
+                <br>
+                <h1>Login</h1>
+                <input type="email" id="email" placeholder="Email">
+                <input type="password" id="password" placeholder="Password">
+                <a href="#">Forget Your Password?</a>
+                <button type="submit">Sign In</button>
             </form>
         </div>
- 
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-right image-row">
+                    <img src="img/logo2.png" alt="logo" width="300" height="300">
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
-        document.getElementById('togglePassword').addEventListener('click', function () {
-            const passwordField = document.getElementById('password');
-            const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-            passwordField.setAttribute('type', type);
-            this.classList.toggle('fa-eye-slash');
-        });
+    function validateForm() {
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        if (email == "" || password == "") {
+            alert("Email dan Password harus diisi!");
+            return false;
+        }
+        return true;
+    }
     </script>
 </body>
-
 </html>
